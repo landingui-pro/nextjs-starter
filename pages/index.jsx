e8bc547904c050d29280da16087f34ed1fb38dec
Krishna Kant <lushkant@gmail.com>
2023-04-20 06:16:31 +0530

    Update files and packages

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,20 +1,20 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 const Home = () => {
-  return (
-    <>
-      <Head>
-        <title>NextJS Starter template with TailwindCSS</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<>
+			<Head>
+				<title>NextJS Starter template with TailwindCSS</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <div className="flex flex-col items-center justify-center py-2">
-        <main className="mx-auto w-full max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8 flex gap-10">
-          <h1 className="bg-gray-200 px-2">Hello World!</h1>
-        </main>
-      </div>
-    </>
-  );
+			<div className="flex flex-col items-center justify-center py-2">
+				<main className="mx-auto w-full max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8 flex gap-10">
+					<h1 className="bg-gray-200 px-2">Hello World!</h1>
+				</main>
+			</div>
+		</>
+	);
 };
 
 export default Home;
